6a1c66af4b517fdcec788185e651be24adc07812
harrisonward1311 <harrisonward1311@gmail.com>
2026-08-20 16:13:57 +0100

    moved utils into common directory

--- src/football_data/main.py
+++ src/football_data/main.py
@@ -1,15 +1,12 @@
 from dotenv import load_dotenv
 import os
 
-from utils import create_spark_session, write_results
+from common.utils import write_results
 from api_functions import fetch_2026_schedule, clean_2026_schedule
 
 # Load the API key from .env
 load_dotenv()
 api_key = os.getenv("FOOTBALL_DATA_API_KEY")
-
-# Create SparkSession
-spark = create_spark_session()
 
 data_schedule_2026 = fetch_2026_schedule(api_key)
 
