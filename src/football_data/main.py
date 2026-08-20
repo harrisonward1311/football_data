9ab701245d790f306ccb03f129eb864abdd61827
harrisonward1311 <harrisonward1311@gmail.com>
2026-08-20 16:11:29 +0100

    orchestration for loading 2026 schedule into JSON

---
+++ src/football_data/main.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+
+from utils import create_spark_session, write_results
+from api_functions import fetch_2026_schedule, clean_2026_schedule
+
+# Load the API key from .env
+load_dotenv()
+api_key = os.getenv("FOOTBALL_DATA_API_KEY")
+
+# Create SparkSession
+spark = create_spark_session()
+
+data_schedule_2026 = fetch_2026_schedule(api_key)
+
+cleaned_schedule_2026 = clean_2026_schedule(data_schedule_2026)
+
+write_results(cleaned_schedule_2026, "output/output_pl_2026_clean.json")
